c62c3104e90aa6b124a617010f0c9c80ded8f3c6
maxiamikel19 <maxloversist19@gmail.com>
2025-04-28 08:24:13 -0400

    Inicial commit

---
+++ src/test/java/com/maxiamikel/age_calculator/AgeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxiamikel.age_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
